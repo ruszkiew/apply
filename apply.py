5bf13799f1013aa658ba3d8e8680b547d0bdb9df
ruszkiew <41125734+ruszkiew@users.noreply.github.com>
2018-12-12 22:07:55 -0600

    Update apply.py

--- apply.py
+++ apply.py
@@ -12,7 +12,8 @@
 # v2.1 - command output was fixed - put direct into list   #
 # v2.0 - added absolute timeout for channel receiving      #
 # v1.9 - added debug boolean for function printouts        #
-# v1.8 - updated SSH without threads  - Tim Way help       #
+# v1.8 - updated SSH without threads  - Tim Way .          #
+# v1.8 - moved SSH back from class                         #
 # v1.7 - using ios_cli_parse to glean variablas to device  #
 # v1.6 - added 'usage' output and checked for argv exist   #
 # v1.5 - created more functions to prep for unit testing   #
