b1098550201648f1ee06f6f0723032a0406ca7b2
Ed Ruszkiewicz <ed.ruszkiewicz@cdw.com>
2019-03-15 18:07:32 -0500

    added pipfile

--- apply.py
+++ apply.py
@@ -13,8 +13,7 @@
 # v2.1 - command output was fixed - put direct into list   #
 # v2.0 - added absolute timeout for channel receiving      #
 # v1.9 - added debug boolean for function printouts        #
-# v1.8 - updated SSH without threads  - Tim Way .          #
-# v1.8 - moved SSH back from class                         #
+# v1.8 - updated SSH without threads  - Tim Way help       #
 # v1.7 - using ios_cli_parse to glean variablas to device  #
 # v1.6 - added 'usage' output and checked for argv exist   #
 # v1.5 - created more functions to prep for unit testing   #
@@ -23,7 +22,7 @@
 # v1.2 - added name resolution                             #
 # v1.1 - added username and password prompt                #
 # v1.0 - initial code based on Cisco PRNE class lab        #
-# v0.0 - named it 'apply' from Craig Weinhold perl script  #
+#      - named it 'apply' from Craig Weinhold perl script  #
 #                                                          # 
 #                                                          # 
 # *************** TODO *******************                 #  
