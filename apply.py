c0cfbcf2b8287a0b106f5eed3bbc3fd0e28447ad
ruszkiew <ed.ruszkiewicz>
2025-07-22 09:18:24 -0500

    cleanup

--- apply.py
+++ apply.py
@@ -1,57 +1,17 @@
 #! /usr/bin/env python3
 
-############################################################
-#                                                          #
-# apply.py    - v2.7    - 12/18/18                         #
-#                                                          #
-# v2.7 - updated socket connection exception               #
-# v2.6 - fixed input for python v2.7 + v3.0                #
-# v2.5 - added to github                                   #
-# v2.4 - added more debug - added /n to command array      #
-# v2.3 - added script entry point                          #
-# v2.2 - moved all prints to () for v3 support             #
-# v2.1 - command output was fixed - put direct into list   #
-# v2.0 - added absolute timeout for channel receiving      #
-# v1.9 - added debug boolean for function printouts        #
-# v1.8 - updated SSH without threads                       #
-# v1.7 - using ios_cli_parse to glean variablas to device  #
-# v1.6 - added 'usage' output and checked for argv exist   #
-# v1.5 - created more functions to prep for unit testing   #
-# v1.4 - moved ssh class to module ssh.py                  #
-# v1.3 - added mutli commands per session                  #
-# v1.2 - added name resolution                             #
-# v1.1 - added username and password prompt                #
-# v1.0 - initial code based on Cisco PRNE class lab        #
-#      - named it 'apply' from Craig Weinhold perl script  #
-#                                                          # 
-#                                                          # 
-# *************** TODO *******************                 #  
-# - allow to pass in variables to glean                    # 
-# - add error correction - component testing               # 
-# - add error correction - debug switch                    # 
-#                                                          # 
-#                                                          # 
-############################################################
+###########################################################
 
-# libraries
-
-# standard modules
 import os
 import socket
 import getpass
 import re
 import sys
 from time import sleep
-
-# external modules
 import paramiko
 from paramiko import SSHClient,AutoAddPolicy
 
-# my modules
-
 ###########################################################
-
-# functions
 
 def cliUsage():
    print(
@@ -223,6 +183,3 @@ if __name__ == "__main__":
             print(line)
 
 ###########################################################
-
-   
-
